4debba4294254716d7cbbf5a5ec09217128bfd56
mohicana <fernandofolchsales@gmail.com>
2021-09-17 22:20:23 +0200

    Fixed minor bug

--- src/com/ffs/Main.java
+++ src/com/ffs/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         int result = 5 + 5;
-        System.out.println("Hola mundo");
+        System.out.println(result);
     }
 }
