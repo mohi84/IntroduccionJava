339550b3c611e547ca5f673ac71aa5d8ab297a89
mohicana <fernandofolchsales@gmail.com>
2021-09-19 12:31:26 +0200

    Added test support

--- src/com/ffs/Main.java
+++ src/com/ffs/Main.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int dividend = 20;
-        int divisor = 6;
-        int finalAddition = 3;
-        System.out.println((dividend / divisor) + finalAddition);
+    public void Hola() {
 
-        Scanner reader = new Scanner(System.in);
-        float side = reader.nextFloat();
-        System.out.println(side);
+    }
+
+    public static void main(String[] args) {
+//        int dividend = 20;
+//        int divisor = 6;
+//        int finalAddition = 3;
+//        System.out.println((dividend / divisor) + finalAddition);
+//
+//        Scanner reader = new Scanner(System.in);
+//        float side = reader.nextFloat();
+//        System.out.println(side);
 
         System.out.println("Otra prueba");
     }
---
+++ src/com/ffs/MainTest.java
@@ -0,0 +1,19 @@
+package com.ffs;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Hola Test")
+    void hola() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    void main() {
+    }
+}
