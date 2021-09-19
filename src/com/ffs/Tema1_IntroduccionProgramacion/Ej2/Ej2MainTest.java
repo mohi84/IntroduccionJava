fa31d4202ebb0b48a4e6f6385f75a582e24a9e57
mohicana <fernandofolchsales@gmail.com>
2021-09-19 18:38:52 +0200

    Ej2 created

--- src/com/ffs/Tema1_IntroduccionProgramacion/Ej2/Ej2MainTest.java
+++ src/com/ffs/Tema1_IntroduccionProgramacion/Ej2/Ej2MainTest.java
@@ -1,10 +1,15 @@
 package com.ffs.Tema1_IntroduccionProgramacion.Ej2;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Ej2MainTest {
 
     @Test
     void main() {
+        assertEquals(2, 2);
+        assertAll("Prueba Multiple",
+                () -> assertTrue(true),
+                () -> assertFalse(false));
     }
 }
